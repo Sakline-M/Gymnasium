c43efce1e9f18819057e800e474678df7db40ae6
sakin <saklinemostaksakin58@gmail.com>
2023-07-29 07:17:54 +0600

    first commit

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Layout from "../../components/Layouts/Layout";
+
+const Home = () => {
+  return (
+    <div>
+      <Layout>
+        <h1>Home</h1>
+      </Layout>
+    </div>
+  );
+};
+
+export default Home;
